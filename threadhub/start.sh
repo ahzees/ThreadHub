3f9d9a83dfab6b4ff94f5a666441084ffeef128a
ahzees <matviy.stets@gmail.com>
2023-12-11 17:07:37 +0200

    quickfix4

--- threadhub/start.sh
+++ threadhub/start.sh
@@ -2,4 +2,4 @@
 python threadhub/manage.py makemigrations authentication
 python threadhub/manage.py makemigrations thread
 python threadhub/manage.py migrate
-python threadhub/manage.py runserver 0.0.0.0:8000
+python threadhub/manage.py runserver
